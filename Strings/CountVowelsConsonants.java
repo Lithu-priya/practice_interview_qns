1af1706284c52791d6d39259f07fadd04e70558b
Lithu-priya <priya26041999@gmail.com>
2026-05-28 17:58:40 +0530

    Add CountVowelsConsonants class to count letters

---
+++ Strings/CountVowelsConsonants.java
@@ -0,0 +1,30 @@
+public class CountVowelsConsonants {
+
+    public static void main(String[] args) {
+
+        String s = "Priyadharshini".toLowerCase();
+
+        int vowels = 0;
+        int consonants = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            if (ch >= 'a' && ch <= 'z') {
+
+                if (ch == 'a' || ch == 'e' || ch == 'i'
+                        || ch == 'o' || ch == 'u') {
+
+                    vowels++;
+
+                } else {
+                    consonants++;
+                }
+            }
+        }
+
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+    }
+}
